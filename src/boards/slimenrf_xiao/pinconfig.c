96bed212840f982c08912dde1c8e6fea7535d453
sctanf <36978460+sctanf@users.noreply.github.com>
2025-06-16 09:36:42 -0500

    revert booloaderConfig

--- src/boards/slimenrf_xiao/pinconfig.c
+++ src/boards/slimenrf_xiao/pinconfig.c
@@ -10,7 +10,7 @@ const uint32_t bootloaderConfig[] =
 
   204, 0x100000,                                // FLASH_BYTES = 0x100000
   205, 0x40000,                                 // RAM_BYTES = 0x40000
-  208, (0x1209 << 16) | 0x7693, // compatibility
+  208, (USB_DESC_VID << 16) | USB_DESC_UF2_PID, // BOOTLOADER_BOARD_ID = USB VID+PID, used for verification when updating bootloader via uf2
   209, 0xada52840,                              // UF2_FAMILY = 0xada52840
   210, 0x20,                                    // PINS_PORT_SIZE = PA_32
 
